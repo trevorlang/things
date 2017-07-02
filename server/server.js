59e4db38fd6bb3afcd5d806e6946d26ca3731144
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 17:38:14 -0400

    Creating server.js

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+const http = require('http');
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8080, 'localhost');
+});
+
+console.log('Server running at http://localhost:8080/');
