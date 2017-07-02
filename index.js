b7672a1a52eeb031ce8c890dfec76be07adad1ec
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 05:15:38 -0400

    Fixing port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 8080;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
