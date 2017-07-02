3f874bac462aedc33ebd866d57ed12c34213cd7f
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 04:21:41 -0400

    Initial commit. Woop.

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
