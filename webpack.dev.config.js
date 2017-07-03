ac7344cf67c4d986bfaa5de3d8679673091d4cf6
Trevor Lang <trevorsaynever@gmail.com>
2017-07-03 17:26:36 -0400

    Configuring webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,4 +1,11 @@
+const path = require('path');
 const webpack = require('webpack');
 const baseConfig = require("./webpack.base.config.js");
+
+baseConfig.devServer = {
+  contentBase: path.join(__dirname, "dist"),
+  compress: true,
+  port: 8080
+};
 
 module.exports = baseConfig;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,14 +1,14 @@
 const webpack = require('webpack');
 const baseConfig = require("./webpack.base.config.js");
 
-// baseConfig.devtool = "cheap-module-source-map";
-//
-// baseConfig.plugins = [
-//     new webpack.DefinePlugin({
-//         "process.env": {
-//             "NODE_ENV": JSON.stringify("production");
-//         }
-//     })
-// ];
+baseConfig.devtool = "cheap-module-source-map";
+
+baseConfig.plugins = [
+    new webpack.DefinePlugin({
+        "process.env": {
+            "NODE_ENV": JSON.stringify("production");
+        }
+    })
+];
 
 module.exports = baseConfig;
