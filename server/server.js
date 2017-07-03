2872ae2daf8109de562050c10b074a33660ec01a
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 21:33:57 -0400

    Tweaking server js

--- server/server.js
+++ server/server.js
@@ -5,18 +5,8 @@ const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
-fs.readFile('./dist/index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-
-    console.log('Creating server at http://localhost:8080/')
-
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(8080, 'localhost');
-});
-
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
