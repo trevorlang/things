678b2e2a83f4061e661ec909d5e1b404b958edb0
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 20:55:10 -0400

    Tweaking server js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env nodejs
+'use strict'
 
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('./dist/index.html', function (err, html) {
     if (err) {
         throw err;
     }
+
+    console.log('Creating server at http://localhost:8080/')
+
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
         response.write(html);
