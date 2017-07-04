1dd4bbc705ef46b02643162d7dd18f510b354e00
Trevor Lang <trevorsaynever@gmail.com>
2017-07-04 00:30:13 -0400

    Webpack tweaks

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,10 +3,4 @@ const baseConfig = require("./webpack.base.config.js");
 
 baseConfig.devtool = "cheap-module-source-map";
 
-baseConfig.plugins = [
-    new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify("production")
-    })
-];
-
 module.exports = baseConfig;
